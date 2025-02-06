f707625dd3b8145e4fb45e8c5f35d4b109e820d4
kirthika85 <kirthika.rajamani@gmail.com>
2025-02-05 23:43:12 -0800

    Update clinicalTrialEligibilityBatch.py

--- clinicalTrialEligibilityBatch.py
+++ clinicalTrialEligibilityBatch.py
@@ -179,7 +179,7 @@ if len(uploaded_files) >= 3 and openai_api_key:
         eligibility_table = []
         for nct_id in nct_numbers:
             selected_patient_row = patient_df[patient_df['Patient Name'] == selected_patient].iloc[0]
-            #st.write("Eligibility for {nct_id}:")
+            st.write(f"Eligibility for {nct_id}:")
                 
             # Fetch and parse criteria for selected trial
             criteria_text = fetch_trial_criteria(nct_id)
@@ -216,7 +216,7 @@ if len(uploaded_files) >= 3 and openai_api_key:
                        'Number of Inclusion Criteria Matches': inclusion_score_numerator
                     })
                else:
-                   st.write("{nct_id} has zero eligibility score.")
+                   st.write(f"{nct_id} has zero eligibility score.")
             else:
                  st.error(f"No eligibility criteria found for {nct_id}.")
            
